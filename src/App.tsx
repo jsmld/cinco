0a63263495396c1e308daea337c9cafba2da0f18
jsmld <samvanerpersroijaards@gmail.com>
2024-04-03 18:19:57 -0300

    Removed unused code, added simple guess input which logs to the console

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { ChangeEvent, FormEvent, useState } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+const [guess, setGuess] = useState('')
+
+const handleSubmit = (e: FormEvent) => {
+  e.preventDefault()
+  console.log({guess})
+  setGuess('')
+}
+
+const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+  setGuess(e.target.value.toUpperCase())
+}
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Cinco</h1>
+      <form className="guess-input-wrapper" onSubmit={handleSubmit}>
+        <label htmlFor="guess-input">Enter guess:</label>
+        <input
+          id="guess-input"
+          type="text"
+          pattern="[a-zA-Z]{5}"
+          title="5 letter word"
+          value={guess}
+          onChange={handleChange}/>
+      </form>
     </>
   )
 }
